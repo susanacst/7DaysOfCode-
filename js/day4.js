60ffbfa721b48608ac8386b2568cdaf7e442c58c
Susana Salazar <susanacst098@gmail.com>
2025-03-01 21:09:15 -0600

    actividad día 5

---
+++ js/day4.js
@@ -0,0 +1,29 @@
+ // dia 4 empieza aqui
+ let numeroRandom = Math.floor(Math.random() * 10);
+
+ let continuarTurno = true;
+ let intentos = 1;
+ let numeroUsuario;
+
+ while (continuarTurno) {
+   numeroUsuario = prompt(
+     "Es un número del 0 al 10, ¿cuál número estaré pensando?"
+   );
+   if (numeroRandom == numeroUsuario) {
+     alert("Felicidades, acertaste al número que estaba pensando.");
+     continuarTurno = false;
+   } else {
+     if (intentos < 3) {
+       alert(
+         `Error, llevas ${intentos} intento(s).`
+       );
+       intentos++;
+       
+     } else if (intentos == 3) {
+       alert(
+         `Error, el número era ${numeroRandom} ¡Suerte a la próxima!.`
+       );
+       continuarTurno = false;
+     }
+   }
+ }
